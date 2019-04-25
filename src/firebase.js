7addfd3a8be651cc63c45f1f51cf0cde3710cf59
mu ali <elshrab90.ma@gamil.com>
2019-04-25 19:09:13 +0200

    first deploy

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyA_y68mYSpKGvHg2rpOCzuSn7--LknQGNo",
+  authDomain: "chat-app-cb7ad.firebaseapp.com",
+  databaseURL: "https://chat-app-cb7ad.firebaseio.com",
+  projectId: "chat-app-cb7ad",
+  storageBucket: "chat-app-cb7ad.appspot.com",
+  messagingSenderId: "231951309364"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+export { db };
